e30eaa863a9bf8f918574703695cf7ef7337a5e0
annguyen <annguyen.it.utc@gmail.com>
2022-08-18 16:46:50 +0700

    Section 6: Ethereum Project Infrastructure

---
+++ ethereum/campaign/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'item retire uncover relief vapor medal know custom utility century blame acquire',
+  'https://rinkeby.infura.io/v3/939864a17ecc4a1db0d7016b7cb2bdad'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -2,8 +2,9 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { abi, evm } = require('../compile');
+// const { abi, evm } = require('../ethereum/lottery/compile');
 
+/**
 describe('Lottery Contract', () => {
   let accounts;
   let lottery;
@@ -123,3 +124,4 @@ describe('Lottery Contract', () => {
     assert(players.length === 0);
   });
 });
+ */
